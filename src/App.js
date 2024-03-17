6e002f048718de5fc01d31343f50e7a9769fc450
Mahish Dino <57224382+mahishdino@users.noreply.github.com>
2024-03-17 13:59:13 +0530

    Added React framework

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import Home from './Components/Home';
+import Header from './Components/Header';
+import About from './Components/About';
+import Services from './Components/Services';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <About />
+      <Services />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div >
+  );
+}
+
+export default App;
