d3e5f3cd3306ebc6a4408a33a968eea2af35e5f9
Mahish Dino <57224382+mahishdino@users.noreply.github.com>
2024-03-17 15:34:17 +0530

    Added some change

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,7 +13,7 @@ function About() {
                     ABOUT <span>Me</span>
                 </h2>
                 <h3>Frontend Developer</h3>
-                <p>Hello! I'm Dhondi Rathan Raj, a dedicated React Native Developer with 4 years of hands-on experience in crafting dynamic and responsive Mobile App applications. Passionate about leveraging cutting-edge technologies and best practices to deliver exceptional user experiences, I specialize in front-end development using React.js and a wide range of complementary tools and libraries.</p>
+                <p>Hello! I'm Dhondi Rathan Raj, a dedicated React Native Developer with 4 years of hands-on experience in crafting dynamic and responsive Mobile App applications. Passionate about leveraging cutting-edge technologies and best practices to deliver exceptional user experiences, I specialize in front-end development using React Native and a wide range of complementary tools and libraries.</p>
                 <p>Skills : <span>HTML, CSS, JavaScript, TypeScript, Tailwind CSS, Node Js,Nest Js,Qwik Js, React Native, Kotlin,Graph ql </span></p>
                 <a href="#" className="btn">Read More</a>
             </div>
